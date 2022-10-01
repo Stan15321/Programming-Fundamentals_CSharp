e07e37522c5362df2b62690abcb784d4f11f4fa2
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-10-01 20:23:27 +0300

    Ex7,8

---
+++ Ex3.Arrays/Ex7.MaxSequenceOfEqualSum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex7.MaxSequenceOfEqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxLength = 0;
+            int length = 1;
+
+            int start = 0;
+            int bestStart = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    length++;
+                }
+                else
+                {
+                    length = 1;
+                    start = i;
+                }
+                if (length > maxLength)
+                {
+                    maxLength = length;
+                    bestStart = start;
+                }
+                
+            }
+
+            for (int i = bestStart; i < bestStart + maxLength; i++)
+            {
+                Console.Write(array[i]+ " ");
+            }
+        }
+    }
+}
---
+++ Ex3.Arrays/Ex8.MagicSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex8.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j < array.Length ; j++)
+                {
+                    if (array[i]+array[j]==n)
+                    {
+                        Console.WriteLine($"{array[i]} {array[j]}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
