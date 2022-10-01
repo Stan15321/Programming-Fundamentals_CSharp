9bbb337c7e77e9baecb2b9347e9a69cdfecb582c
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-10-01 15:05:23 +0300

    working on ex5,6

---
+++ Ex3.Arrays/Ex5.TopInteger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex5.TopInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < integers.Length; i++)
+            {
+                int currNumber = integers[i];
+                bool isTopInteger = true;
+                for (int j = i+1; j < integers.Length; j++)
+                {
+                    int nextNumber = integers[j];
+                    if (nextNumber>=currNumber)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    Console.Write(currNumber + " ");
+                }
+            }
+        }
+    }
+}
---
+++ Ex3.Arrays/Ex6.EqualSums/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex6.EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int sumOnTheLeft = 0;
+            int sumOnTheRight = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers.Length==1)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+                sumOnTheLeft = 0;
+                for (int sumLeft= i; sumLeft >0 ; sumLeft--)
+                {
+                    int nextLeftElement = sumLeft - 1;
+                    if (sumLeft > 0)
+                    {
+                        sumOnTheLeft += numbers[nextLeftElement];
+                    }
+                }
+
+                sumOnTheRight = 0;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    int nextElementPosition = j + 1;
+                    if (j< numbers.Length-1)
+                    {
+                        sumOnTheRight += numbers[nextElementPosition];
+                    }
+              
+                }
+                if (sumOnTheRight==sumOnTheLeft)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+
+        }
+    }
+}
